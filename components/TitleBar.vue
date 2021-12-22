fd45c6465dd9cdfd98cb4d31837596645bd457f3
eadiyatma <efran.adiyatma9g@gmail.com>
2021-12-22 08:37:35 +0700

    add title bar

---
+++ components/TitleBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- title section -->
+    <div class="text-2xl font-bold">{{ title }}</div>
+    <!-- subtitle section -->
+    <div class="text-gray-500">{{ subtitle }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleBar',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    subtitle: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
